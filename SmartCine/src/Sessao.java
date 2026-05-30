7a1d380c5a0996e1f911b44c0fcd1baa704b5e9f
Marcelo Rodrigues Rosa Paschoal <marcrodprv@gmail.com>
2026-05-30 14:23:36 -0300

    Criei a classe Assento e desenvolvi Filme e Sessão

--- SmartCine/src/Sessao.java
+++ SmartCine/src/Sessao.java
@@ -1,11 +1,36 @@
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
 
 public class Sessao {
-    
-    public Sessao(Filme filme) {
+    private Filme filme;
+    private String horario;
+    private int idSala;
+    private Assento[][] sala;
+
+    public Sessao(Filme filme, String horario, int idSala) {
+        this.filme = filme;
+        this.horario = horario;
+        this.idSala = idSala;
+
+        sala = new Assento[20][20];
+        for(int i=0; i<20; i++){
+            for(int j=0; j<20; j++){
+                sala[i][j] = new Assento(i, j);
+            }
+        }
     }
     
-    public void reservarAssento(int numeroAssento) throws AssentoIndisponivelException {
+    public void reservarAssento(String idAssento) throws AssentoIndisponivelException {
+        for(int i=0; i<20; i++){
+            for(int j=0; j<20; j++){
+                if(Objects.equals(sala[i][j].getId(), idAssento) && !sala[i][j].isOcupado()){
+                    sala[i][j].setOcupado(true);
+                    return;
+                }
+            }
+        }
+        throw new AssentoIndisponivelException("Assento não encontrado ou já ocupado");
     }
-    
 }
